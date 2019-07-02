dd40fe2c40331b67695aecd6a736152bcbab5f68
Jose Medina <manuel7923@gmail.com>
2019-07-02 10:41:47 -0500

    Creamos el proceso

--- src/main/java/Prueba.java
+++ src/main/java/Prueba.java
@@ -1,8 +1,11 @@
 
 public class Prueba {
-
+    
     public static void main(String[] args) {
         System.out.println("The first repository");
+        first primer = new first();
+        primer.proceso();
     }
+    
     
 }
---
+++ src/main/java/first.java
@@ -0,0 +1,9 @@
+
+public class first {
+    int b=1, c=2;
+    double a=b/c;
+    public void proceso(){
+    System.out.println(a);
+    }    
+    
+}
